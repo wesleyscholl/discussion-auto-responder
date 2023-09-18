e7e5d0d81ba3c5b694b4b8724ffe18137610bc90
Wesley Scholl <wscholl@totalwine.com>
2023-09-18 09:50:25 -0400

    Added new workflow to label prs

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log('Hello World!');
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
+async function run() {
+    var _a;
+    const token = (0, core_1.getInput)('gh-token');
+    const label = (0, core_1.getInput)('label');
+    const octokit = (0, github_1.getOctokit)(token);
+    const pullRequest = github_1.context.payload.pull_request;
+    try {
+        if (!pullRequest) {
+            throw new Error('This action can only be run on Pull Requests');
+        }
+        await octokit.rest.issues.addLabels({
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label],
+        });
+    }
+    catch (error) {
+        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : 'Unknown error');
+    }
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import { getInput } from '@actions/core';
+import { getInput, setFailed } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
 
-const name = getInput('name');
+async function run() {
+  const token = getInput('gh-token');
+  const label = getInput('label');
 
-console.log(`Hello ${name}!`);
+  const octokit = getOctokit(token);
+  const pullRequest = context.payload.pull_request;
+
+  try {
+    if (!pullRequest) {
+      throw new Error('This action can only be run on Pull Requests');
+    }
+
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+  } catch (error) {
+    setFailed((error as Error)?.message ?? 'Unknown error');
+  }
+}
+
+run();
